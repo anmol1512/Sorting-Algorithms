1402c9a6bc8ac177b719f0eea026b10f9a7b801a
Anmol Chhetri <anmol1512@gmail.com>
2021-05-06 17:58:27 +0530

    Binary_search program added v2

--- Count_sort.c
+++ Count_sort.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #define n 10
 #define k 10
+/*Here k is called the range like here in the main unsorted array we have numbers from range 0-9 
+count sort can also be used to sort letters.
+radix sort can only be used on array that contains numbers because it uses count sort on the digits of each numbers in the array
+Radix sort uses counting sort as a subroutine.*/
 int main(){
 	int b[n];
 	int a[n]={1,6,3,6,8,5,2,7,9,9};
